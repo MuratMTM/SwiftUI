d4ba32f23d6356e4ca3c2837683f4de092d623a4
MuratMTM <murat.postmen@gmail.com>
2023-07-04 19:54:15 +0300

    Initial Commit

---
+++ Starting/StartingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartingApp.swift
+//  Starting
+//
+//  Created by Murat Işık on 4.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct StartingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
