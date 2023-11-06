1f402e5bec10f8da3f91526ccdce4a032bdacc59
MuratMTM <murat.postmen@gmail.com>
2023-11-06 17:25:23 +0300

    refactoring the codes

--- Starting/View/ComponentView/SliderView.swift
+++ Starting/View/ComponentView/SliderView.swift
@@ -12,19 +12,20 @@ import Kingfisher
 
 struct SliderView: View {
     
-    @State private var agno: Double = 2.7
+    @State private var agno: Double = 2.8
     @State private var isEditing = false
-    
+    let studentName: String = "Murat Işık\nMatematik Mühendisliği"
+    let photoUrl: String = "https://instagram.fist13-1.fna.fbcdn.net/v/t51.2885-19/394001179_1104824210497833_981005681208049551_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fist13-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=1j3KkNvmDGgAX8egvW2&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfArMvn-r_KDEuI8EqMhdLeHRCiFXtDOdSqkkyQvnXCmdQ&oe=654E8F84&_nc_sid=8b3546"
     var body: some View {
         VStack{
             HStack{
-                Text("Ceyda Bay\nMekatronik Mühendisliği")
+                Text(studentName)
                     .foregroundColor(.pink)
                     .bold()
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 20)
-                    
-                KFImage(URL(string: "https://instagram.fist13-1.fna.fbcdn.net/v/t51.2885-19/371989089_1029247321404035_3426742590501937681_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fist13-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=ZEnn7m3q87kAX8KwEUw&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfDJnSPPV86PndtbLFZIm9r9cRMUtFKJ02wrMa_YPq0gKg&oe=64FB5801&_nc_sid=8b3546"))
+                
+                KFImage(URL(string:photoUrl ))
                     .resizable()
                     .frame(width: 80, height: 80, alignment: .center)
                     .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
@@ -38,24 +39,24 @@ struct SliderView: View {
                 .padding(.vertical, 1)
                 .foregroundColor(.teal)
                 .bold()
+            
+            Text(String(format: "%.2f", agno))
+                .foregroundColor(.purple)
+                .bold()
+            
+            Slider(
+                value: $agno,
+                in: 0...4,
                 
-                Text(String(format: "%.2f", agno))
-                    .foregroundColor(.purple)
-                    .bold()
-                
-                Slider(
-                    value: $agno,
-                    in: 0...4,
-                    
-                    onEditingChanged: { editing in
-                        isEditing = editing
-                    }
-                )
-                Text("\(agno)")
-                    .foregroundColor(isEditing ? .red : .blue)
-                
-            }
+                onEditingChanged: { editing in
+                    isEditing = editing
+                }
+            )
+            Text("\(agno)")
+                .foregroundColor(isEditing ? .red : .blue)
+            
         }
+    }
     
     struct SliderView_Previews: PreviewProvider {
         static var previews: some View {
