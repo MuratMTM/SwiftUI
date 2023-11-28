4fe31943aeb6ec36859e91225bb25dd06db46154
MuratMTM <murat.postmen@gmail.com>
2023-11-28 21:55:05 +0300

    refactoring on the View is done

---
+++ Starting/View/ComponentView/LazyVGridView.swift
@@ -0,0 +1,18 @@
+//
+//  LazyVGridView.swift
+//  Starting
+//
+//  Created by Murat Işık on 25.11.2023.
+//
+
+import SwiftUI
+
+struct LazyVGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyVGridView()
+}
