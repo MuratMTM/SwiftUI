d604ec1e69798791bc97da49f4021888b5fd779c
MuratMTM <murat.postmen@gmail.com>
2023-11-30 19:27:21 +0300

    the property practises are done

---
+++ Starting/View/ComponentView/StatePropertyView.swift
@@ -0,0 +1,18 @@
+//
+//  StatePropertyView.swift
+//  Starting
+//
+//  Created by Murat Işık on 30.11.2023.
+//
+
+import SwiftUI
+
+struct StatePropertyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatePropertyView()
+}
