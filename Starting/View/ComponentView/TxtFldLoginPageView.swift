19475c682cd09b7fd0ed7f69f78874f7c43ede08
MuratMTM <murat.postmen@gmail.com>
2024-01-11 01:42:52 +0300

    login page design and practise on textfield

---
+++ Starting/View/ComponentView/TxtFldLoginPageView.swift
@@ -0,0 +1,76 @@
+//
+//  TxtFldLoginPageView.swift
+//  Starting
+//
+//  Created by Murat Işık on 11.01.2024.
+//
+
+import SwiftUI
+
+
+struct TxtFldLoginPageView: View {
+  
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    
+  
+    var body: some View {
+        ZStack{
+            Image("books")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Image("profilePhoto")
+                    .resizable()
+                    .frame(width: 200,height: 200, alignment: .center)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay{
+                        Circle().stroke(.yellow,lineWidth: 5)
+                    }
+                    .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
+                
+                Text("kesitOkur")
+                    .fontWeight(.bold)
+                    .italic()
+                    .font(.largeTitle)
+                    .padding(.bottom,80)
+                    
+                
+                    
+                    
+                TextField("Kullanıcı Adı", text: $username)
+                    .padding(20)
+                    .background(Color.white.cornerRadius(10))
+                
+                SecureField("Şifre", text: $password)
+                    .padding(20)
+                    .background(Color.white.cornerRadius(10))
+                Spacer()
+                
+                Button(action: {
+                              
+                               print("Kullanıcı Adı: \(username), Şifre: \(password)")
+                }) {
+                               Text("Giriş Yap")
+                        .frame(maxWidth: 200)
+                                   .padding()
+                                   .foregroundColor(.white)
+                                   .background(Color.yellow.opacity(50))
+                                   .cornerRadius(20)
+                                   
+                }.padding()
+                Spacer()
+                    
+            }.padding(.all, 10)
+        }
+    }
+    
+   
+}
+
+#Preview {
+    TxtFldLoginPageView()
+}
