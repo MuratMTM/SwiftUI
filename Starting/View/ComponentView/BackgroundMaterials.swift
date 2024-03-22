135d149fa039bb787acdb84b9756b9b41d3d860d
MuratMTM <murat.postmen@gmail.com>
2024-03-22 03:23:18 +0300

    Implemented material styles for background like ultra thin light.

---
+++ Starting/View/ComponentView/BackgroundMaterials.swift
@@ -0,0 +1,42 @@
+//
+//  BackgroundMaterials.swift
+//  Starting
+//
+//  Created by Murat Işık on 22.03.2024.
+//
+
+import SwiftUI
+
+struct BackgroundMaterials: View {
+    var body: some View {
+        VStack{
+            Spacer()
+            VStack{
+                RoundedRectangle( cornerRadius: 10)
+                    .frame(width: 50, height: 4)
+                
+                    .padding()
+                
+                Spacer()
+                    
+                    
+                    
+                   
+            }
+            .frame(height: 300)
+            .frame(maxWidth: .infinity)
+            .background(.ultraThinMaterial)
+           
+        }.ignoresSafeArea()
+            
+            .background(
+            Image("papara4")
+                .resizable()
+                .frame(height: 870)
+            )
+    }
+}
+
+#Preview {
+    BackgroundMaterials()
+}
